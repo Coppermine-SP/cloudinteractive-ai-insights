b5b5fa00e50d432488dca3aeb67dfecde45780ea
Coppermine-SP <son5121502@gmail.com>
2023-10-30 17:18:15 +0900

    feat: CustomPrompt, __LoadStream

--- tasks.py
+++ tasks.py
@@ -1,24 +1,21 @@
 # cloudinteractive-ai-insights / actions
 # Copyright (C) 2023 CloudInteractive.
 import argparse
+import io
+
 import document
 import azure_api
 import openai_api
+from typing import List
+from typing import Optional
 
 
 def QuestionTranscript(args: argparse.Namespace):
     print("Task: QuestionTranscript")
 
     role = "당신은 문제지 출제자입니다. 문서를 OCR한 문자열에서 각 문제를 띄워쓰기와 문법을 보정하고 요약하여 문제만 줄 단위로 정리하시오.문제에 번호는 붙이지 않는다."
-    if args.filename[0].endswith(".pdf"):
-        stream = document.ConvertToImageStreams(list(map(int, args.pages)), args.filename[0])
-        if not args.pages:
-            print("[ERROR]: pages option is required!")
-            return
-    elif args.filename[0].endswith(".png"):
-        stream = document.LoadImagetoStream(args.filename[0])
-    else:
-        print("[ERROR]: Unsupported file type.")
+    stream = __loadStream(args)
+    if stream is None:
         return
 
     text = azure_api.OCRFromImageStreams(stream, args.verbose)
@@ -48,6 +45,37 @@ def PageSummary(args: argparse.Namespace):
 def CodeExtrect(args: argparse.Namespace):
     pass
 
+def CustomPrompt(args: argparse.Namespace):
+    print("Task: CustomPrompt")
+    stream = __loadStream(args)
+    if stream is None:
+        return
+
+    if not args.prompt:
+        print("[ERROR]: prompt option is required!")
+        return
+    role = args.prompt[0]
+    text = azure_api.OCRFromImageStreams(stream, args.verbose)
+    response = openai_api.ChatCompletion(text, role, openai_api.OpenAIModel.gpt_4, args.verbose)
+    if response is None:
+        return
+
+    print("Result:")
+    for text in response:
+        print(f"\n{text}\n")
+    print("Complete!")
+
+def __loadStream(args: argparse.Namespace) -> Optional[List[io.BytesIO]]:
+    if args.filename[0].endswith(".pdf"):
+        if not args.pages:
+            print("[ERROR]: pages option is required!")
+            return None
+        return document.ConvertToImageStreams(list(map(int, args.pages)), args.filename[0])
+    elif args.filename[0].endswith(".png") or args.filename[0].endswith(".jpg"):
+        return document.LoadImagetoStream(args.filename[0])
+    else:
+        print("[ERROR]: Unsupported file type.")
+        return None
 
 def __questionNormalization(string: str) -> str:
     quotes = ["'", '"', "`"]
@@ -62,4 +90,4 @@ def __questionNormalization(string: str) -> str:
 
 
 global actions
-actions = {"QuestionTranscript": QuestionTranscript, "PageSummary": PageSummary, "CodeExtract": CodeExtrect}
+actions = {"QuestionTranscript": QuestionTranscript, "PageSummary": PageSummary, "CodeExtract": CodeExtrect, "CustomPrompt" : CustomPrompt}
