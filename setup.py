2465e8ce16ef6dde76afded7700c0dd1834e44e9
Coppermine-SP <son5121502@gmail.com>
2023-10-31 15:59:41 +0900

    fix: console script entry_point not work

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cloudinteractive-ai-insights",
+    name="cloudinteractive_ai_insights",
     version="1.0.0",
     description="Collection of AI tools designed to assist with your assignments and projects.",
     author="CoppermineSP",
@@ -9,7 +9,6 @@ setup(
     url="https://github.com/Coppermine-SP/cloudinteractive-ai-insights",
     install_requires=["azure-cognitiveservices-vision-computervision","openai","pdf2image","tiktoken"],
     python_requires=">=3.9",
-    zip_safe=False,
     classifiers=[
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
@@ -17,7 +16,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'ai-insight = cloudinteractive-ai-insights.main:main',
-        ],
+            'ai-insights=cloudinteractive_ai_insights.main:main',
+        ]
     }
 )
