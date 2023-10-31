2028dc022c019593b96026fd4977c7d8e769e661
Coppermine-SP <son5121502@gmail.com>
2023-10-31 19:42:49 +0900

    feat: PyPI package long description, license add

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup, find_packages
 
+try:
+    with open('README.md', encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    long_description = ""
+
 setup(
     name="cloudinteractive_ai_insights",
-    version="1.0.2",
+    version="1.0.4",
     description="Collection of AI tools designed to assist with your assignments and projects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="CoppermineSP",
+    license="MIT",
     author_email="copperminesp@cloudinteractive.net",
     url="https://github.com/Coppermine-SP/cloudinteractive-ai-insights",
-    install_requires=["azure-cognitiveservices-vision-computervision","openai","pdf2image","tiktoken"],
+    install_requires=["azure-cognitiveservices-vision-computervision", "openai", "pdf2image", "tiktoken"],
     python_requires=">=3.9",
     classifiers=[
         'Programming Language :: Python :: 3.9',
